421f3c1fc36d7971e9fb3d550e4682517efceeda
akshajnad <98061663+akshajnad@users.noreply.github.com>
2025-01-04 01:16:23 -0500

    Create simRender.py

---
+++ simRender.py
@@ -0,0 +1,3 @@
+#env.run(["submission.py", "submission.py", "submission.py", "submission.py"])
+env.run(["submission.py", "random", "random", "random"])
+env.render(mode="ipython", width=800, height=600)
